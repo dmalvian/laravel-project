0487c63852452a0b23cda5cf75e3b16b2a43697f
Awan <hendrawanss123@gmail.com>
2018-05-15 10:52:07 +0700

    Commit pertama

---
+++ resources/views/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+  </head>
+  <body>
+  @yield('content')
+  </body>
+</html>
